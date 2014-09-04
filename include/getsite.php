919279e224565fb9b626debd3d67b4f49b34b644
Snowstormer <snowstormer@users.noreply.github.com>
2014-09-04 18:51:59 +0300

    I'm an idiot

--- include/getsite.php
+++ include/getsite.php
@@ -1,14 +1,6 @@
 <?php
-function endsWith($haystack, $needle)
-{
-    return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
-}
 $DIR = $_SERVER['PHP_SELF'];
 $DIR = explode("/", $DIR);
-if (endsWith($DIR[1], ".php")) {
-	$IURL = "http://".$_SERVER['HTTP_HOST']."/";
-} else {
-	$IURL = "http://".$_SERVER['HTTP_HOST']."/".$DIR[1]."/";
-}
+$IURL = "http://".$_SERVER['HTTP_HOST']."/";
 $ID = getcwd()."/";
 ?>
