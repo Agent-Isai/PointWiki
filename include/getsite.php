c9f32538e66b111d2c750713760dd1ace7e77548
Snowstormer <snowstormer@users.noreply.github.com>
2014-09-04 18:42:06 +0300

    fix getsite.php

--- include/getsite.php
+++ include/getsite.php
@@ -1,6 +1,14 @@
 <?php
+function endsWith($haystack, $needle)
+{
+    return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
+}
 $DIR = $_SERVER['PHP_SELF'];
 $DIR = explode("/", $DIR);
-$IURL = "http://".$_SERVER['HTTP_HOST']."/".$DIR[1]."/";
+if (endsWith($DIR[1], ".php")) {
+	$IURL = "http://".$_SERVER['HTTP_HOST']."/";
+} else {
+	$IURL = "http://".$_SERVER['HTTP_HOST']."/".$DIR[1]."/";
+}
 $ID = getcwd()."/";
 ?>
