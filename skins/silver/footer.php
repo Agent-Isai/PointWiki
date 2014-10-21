17355baf6ab336e94d67c1362f9d472c6c0f92f8
Snowstormer <Snowstormer@users.noreply.github.com>
2014-10-21 22:04:47 +0300

    Update footer.php

--- skins/silver/footer.php
+++ skins/silver/footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
 <hr>
 <?php
-	echo '<img class="poweredby" src="http://www.snowstormer.tk/wiki/skins/common/PointWikiBanner.png" width="88px" height="31px">';
+	echo '<img class="poweredby" src="'.$ID.'"skins/common/PointWikiBanner.png" width="88px" height="31px">';
 	echo '<img class="license" src="http://i.creativecommons.org/l/'.$licensefile.'/3.0/88x31.png">';
 	echo '<div class="content">Contributions to '.$name.' are licensed under '.$licensefull.'.</div>';
 ?>
